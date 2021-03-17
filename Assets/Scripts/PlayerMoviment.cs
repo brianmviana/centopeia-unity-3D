ee6891e05aaebd6db474cf30e42210b6c701db80
Brian Viana <brianmviana@gmail.com>
2021-03-17 08:45:55 -0300

    third person movement

--- Assets/Scripts/PlayerMoviment.cs
+++ Assets/Scripts/PlayerMoviment.cs
@@ -4,36 +4,75 @@ using UnityEngine;
 
 public class PlayerMoviment : MonoBehaviour {
 
-    public float MoveSpeed;
-    public float TurnSpeed;
+    /*    public float MoveSpeed;
+        public float TurnSpeed;
+        public float Velocity;
+*/
+        public Rigidbody rigidbody;
 
-    public Rigidbody rigidbody;
+/*    public CharacterController controller;*/
+    public Transform cam;
+    public float speed = 6f;
+    public float turnSmoothTime = 0.1f;
+    float turnSmoothVelocity;
 
     void Start() {
-        MoveSpeed = 5f;
-        TurnSpeed = 90f;
-}
+        /*        MoveSpeed = 10f;
+                TurnSpeed = 90f;*/
 
+    }
     void Update() {
-        if (Input.GetKey(KeyCode.W)) {
-            walking(1f, 0f);
-        }
-        if (Input.GetKey(KeyCode.S)) {
-            walking(-1f, 0f);
-        }
-        if (Input.GetKey(KeyCode.A)) {
-            walking(0f, -1f);
-        }
-        if (Input.GetKey(KeyCode.D)) {
-            walking(0f, 1f);
+        float horizontal = Input.GetAxisRaw("Horizontal");
+        float vertical = Input.GetAxisRaw("Vertical");
+
+        Vector3 direction = new Vector3(horizontal, 0f, vertical).normalized;
+
+        if (direction.magnitude >= 0.1f) {
+            float targeAngle = Mathf.Atan2(direction.x, direction.z) * Mathf.Rad2Deg + cam.eulerAngles.y;
+            float angle = Mathf.SmoothDampAngle(transform.eulerAngles.y, targeAngle, ref turnSmoothVelocity, turnSmoothTime);
+            transform.rotation = Quaternion.Euler(0f, angle, 0f);
+            Vector3 moveDir = Quaternion.Euler(0f, targeAngle, 0f) * Vector3.forward;
+            transform.Translate(direction * speed * Time.deltaTime);
         }
     }
 
-    private void walking(float direction_forward, float direction_right) {
+}
+
+        /*
+            void Update() {
+                        if (Input.GetKey(KeyCode.W)) {
+                            walking(1f, 0f);
+                        }
+                        if (Input.GetKey(KeyCode.S)) {
+                            walking(-1f, 0f);
+                        }
+                        if (Input.GetKey(KeyCode.A)) {
+                            walking(0f, -1f);
+                        }
+                        if (Input.GetKey(KeyCode.D)) {
+                            walking(0f, 1f);
+                        }
+
+                        if (Input.GetKey(KeyCode.LeftArrow)) {
+                            rotate(TurnSpeed);
+                        }
+                        if (Input.GetKey(KeyCode.RightArrow)) {
+                            rotate(-TurnSpeed);
+                        }
+                    }
+                */
+
+
+
+     /*   private void walking(float direction_forward, float direction_right) {
         Vector3 targetMovement = new Vector3(
             (transform.right * MoveSpeed * Time.deltaTime * direction_right).x, 0f,
-            (transform.forward * MoveSpeed * Time.deltaTime * direction_forward).z);        
-
-        rigidbody.MovePosition(rigidbody.position + targetMovement);
+            (transform.forward * MoveSpeed * Time.deltaTime * direction_forward).z);
+        transform.Translate(targetMovement);
+//        rigidbody.MovePosition(rigidbody.position + targetMovement);
     }
-}
+
+    private void rotate(float TurnSpeed) {
+        transform.Rotate(0, TurnSpeed * Time.deltaTime * 1f, 0);
+    }
+}*/
