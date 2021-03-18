36acc519e19b46299429763b0f4746be78cb8652
Brian M. Viana <brianmviana@gmail.com>
2021-03-18 18:05:55 -0300

    Crosshair point

---
+++ Assets/Scripts/PlayerCrosshair.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+
+public class PlayerCrosshair : MonoBehaviour {
+
+    public bool firstPerson;
+    public GameObject mira;
+    public GameObject crosshairPoint;
+    public float shootForce;
+    private Ray ray;
+    RaycastHit raycastHit;
+
+    void Update(){
+        ray = new Ray(mira.transform.position, mira.transform.forward);
+        if (Physics.Raycast(ray, out RaycastHit raycastHit)) {
+            this.raycastHit = raycastHit;
+            crosshairPoint.transform.position = raycastHit.point;
+        }
+        if (Input.GetMouseButtonDown(0)) {
+            atira();
+        }
+    }
+
+    private void atira() {
+        if (this.raycastHit.rigidbody) {
+            this.raycastHit.rigidbody.AddForceAtPosition(ray.direction * shootForce, this.raycastHit.point);
+        }
+    }
+}
--- Assets/Scripts/PlayerMoviment.cs
+++ Assets/Scripts/PlayerMoviment.cs
@@ -1,78 +1,93 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
 public class PlayerMoviment : MonoBehaviour {
 
-    /*    public float MoveSpeed;
-        public float TurnSpeed;
-        public float Velocity;
-*/
-        public Rigidbody rigidbody;
+    public float MoveSpeed;
+    public float TurnSpeed;
+    public float Velocity;
+    public float JumpForce = 300f;
+    private bool _jumping = false;
 
-/*    public CharacterController controller;*/
+    public Rigidbody rigidbody;
+    public GameObject piso;
+
     public Transform cam;
-    public float speed = 6f;
+
+    /*    public CharacterController controller;
+        public Transform cam;
+        public float speed = 6f;
+        public float turnSmoothTime = 0.1f;
+        float turnSmoothVelocity;
+    */
     public float turnSmoothTime = 0.1f;
     float turnSmoothVelocity;
 
+
     void Start() {
-        /*        MoveSpeed = 10f;
-                TurnSpeed = 90f;*/
+        MoveSpeed = 10f;
+        TurnSpeed = 90f;
 
     }
+
+    /*    void Update() {
+            float horizontal = Input.GetAxisRaw("Horizontal");
+            float vertical = Input.GetAxisRaw("Vertical");
+
+            Vector3 direction = new Vector3(horizontal, 0f, vertical).normalized;
+
+            if (direction.magnitude >= 0.1f) {
+                float targeAngle = Mathf.Atan2(direction.x, direction.z) * Mathf.Rad2Deg + cam.eulerAngles.y;
+                float angle = Mathf.SmoothDampAngle(transform.eulerAngles.y, targeAngle, ref turnSmoothVelocity, turnSmoothTime);
+                transform.rotation = Quaternion.Euler(0f, angle, 0f);
+                Vector3 moveDir = Quaternion.Euler(0f, targeAngle, 0f) * Vector3.forward;
+                transform.Translate(direction * speed * Time.deltaTime);
+            }
+        }*/
+
+
+
     void Update() {
-        float horizontal = Input.GetAxisRaw("Horizontal");
-        float vertical = Input.GetAxisRaw("Vertical");
+        if (Input.GetKey(KeyCode.W)) {
+            walking(1f, 0f);
+        }
+        if (Input.GetKey(KeyCode.S)) {
+            walking(-1f, 0f);
+        }
+        if (Input.GetKey(KeyCode.A)) {
+            walking(0f, -1f);
+        }
+        if (Input.GetKey(KeyCode.D)) {
+            walking(0f, 1f);
+        }
 
-        Vector3 direction = new Vector3(horizontal, 0f, vertical).normalized;
-
-        if (direction.magnitude >= 0.1f) {
-            float targeAngle = Mathf.Atan2(direction.x, direction.z) * Mathf.Rad2Deg + cam.eulerAngles.y;
-            float angle = Mathf.SmoothDampAngle(transform.eulerAngles.y, targeAngle, ref turnSmoothVelocity, turnSmoothTime);
-            transform.rotation = Quaternion.Euler(0f, angle, 0f);
-            Vector3 moveDir = Quaternion.Euler(0f, targeAngle, 0f) * Vector3.forward;
-            transform.Translate(direction * speed * Time.deltaTime);
+        if (Input.GetKey(KeyCode.LeftArrow)) {
+            rotate(-TurnSpeed);
+        }
+        if (Input.GetKey(KeyCode.RightArrow)) {
+            rotate(TurnSpeed);
         }
     }
 
-}
-
-        /*
-            void Update() {
-                        if (Input.GetKey(KeyCode.W)) {
-                            walking(1f, 0f);
-                        }
-                        if (Input.GetKey(KeyCode.S)) {
-                            walking(-1f, 0f);
-                        }
-                        if (Input.GetKey(KeyCode.A)) {
-                            walking(0f, -1f);
-                        }
-                        if (Input.GetKey(KeyCode.D)) {
-                            walking(0f, 1f);
-                        }
-
-                        if (Input.GetKey(KeyCode.LeftArrow)) {
-                            rotate(TurnSpeed);
-                        }
-                        if (Input.GetKey(KeyCode.RightArrow)) {
-                            rotate(-TurnSpeed);
-                        }
-                    }
-                */
-
-
-
-     /*   private void walking(float direction_forward, float direction_right) {
-        Vector3 targetMovement = new Vector3(
-            (transform.right * MoveSpeed * Time.deltaTime * direction_right).x, 0f,
-            (transform.forward * MoveSpeed * Time.deltaTime * direction_forward).z);
-        transform.Translate(targetMovement);
-//        rigidbody.MovePosition(rigidbody.position + targetMovement);
+    private void walking(float direction_forward, float direction_right) {
+        float x = (transform.right.x * MoveSpeed * Time.deltaTime * direction_right);
+        float y = 0f;
+        float z = (transform.forward.z * MoveSpeed * Time.deltaTime * direction_forward);
+        Vector3 direction = new Vector3(x, y, z);
+        rigidbody.MovePosition(rigidbody.position + direction);
     }
 
     private void rotate(float TurnSpeed) {
         transform.Rotate(0, TurnSpeed * Time.deltaTime * 1f, 0);
+
     }
-}*/
+
+    /*    private void Jump() {
+            rigidbody.AddForce(0, JumpForce, 0);
+        }
+
+        private void OnCollisionStay(Collision collision) {
+            if (collision.gameObject.CompareTag(GameTags.Plaform) && Input.GetKeyDown(KeyCode.Space)) {
+                Jump();
+            }
+        }*/
+    }
