e045c6a082cdd36bc3a62157ed7e7995aa8d4880
Brian M. Viana <brianmviana@gmail.com>
2021-03-22 18:06:03 -0300

    new movement to first player and guns

--- Assets/Scripts/PlayerMoviment.cs
+++ Assets/Scripts/PlayerMoviment.cs
@@ -2,52 +2,48 @@
 
 public class PlayerMoviment : MonoBehaviour {
 
-    public float MoveSpeed;
-    public float TurnSpeed;
-    public float Velocity;
-    public float JumpForce = 300f;
-    private bool _jumping = false;
+    public CharacterController controller;
+    
+    public float speed = 12f;
+    public float gravity = -9.81f;
+    public float jumpHeight = 1f;
 
-    public Rigidbody rigidbody;
-    public GameObject piso;
+    public Transform groundCheck;
+    public float GroundDistance = 0.4f;
+    public LayerMask groundMask;
 
-    public Transform cam;
-
-    /*    public CharacterController controller;
-        public Transform cam;
-        public float speed = 6f;
-        public float turnSmoothTime = 0.1f;
-        float turnSmoothVelocity;
-    */
-    public float turnSmoothTime = 0.1f;
-    float turnSmoothVelocity;
-
+    Vector3 velocity;
+    bool isGrounded;
 
     void Start() {
-        MoveSpeed = 10f;
-        TurnSpeed = 90f;
-
     }
-
-    /*    void Update() {
-            float horizontal = Input.GetAxisRaw("Horizontal");
-            float vertical = Input.GetAxisRaw("Vertical");
-
-            Vector3 direction = new Vector3(horizontal, 0f, vertical).normalized;
-
-            if (direction.magnitude >= 0.1f) {
-                float targeAngle = Mathf.Atan2(direction.x, direction.z) * Mathf.Rad2Deg + cam.eulerAngles.y;
-                float angle = Mathf.SmoothDampAngle(transform.eulerAngles.y, targeAngle, ref turnSmoothVelocity, turnSmoothTime);
-                transform.rotation = Quaternion.Euler(0f, angle, 0f);
-                Vector3 moveDir = Quaternion.Euler(0f, targeAngle, 0f) * Vector3.forward;
-                transform.Translate(direction * speed * Time.deltaTime);
-            }
-        }*/
-
 
 
     void Update() {
-        if (Input.GetKey(KeyCode.W)) {
+        isGrounded = Physics.CheckSphere(groundCheck.position, GroundDistance, groundMask);
+
+        if (isGrounded && velocity.y < 0) {
+            velocity.y = -2f;
+        }
+
+        float x = Input.GetAxis("Horizontal");
+        float z = Input.GetAxis("Vertical");
+
+        Vector3 move = transform.right * x + transform.forward * z;
+
+        controller.Move(move * speed * Time.deltaTime);
+
+        /*if (Input.GetButtonDown("Jump") && isGrounded) {
+            velocity.y = Mathf.Sqrt(jumpHeight * -2f * gravity);
+        }*/
+
+        velocity.y += gravity * Time.deltaTime;
+        controller.Move(velocity * Time.deltaTime);
+
+        
+
+        
+        /*if (Input.GetKey(KeyCode.W)) {
             walking(1f, 0f);
         }
         if (Input.GetKey(KeyCode.S)) {
@@ -58,36 +54,14 @@ public class PlayerMoviment : MonoBehaviour {
         }
         if (Input.GetKey(KeyCode.D)) {
             walking(0f, 1f);
-        }
+        }*/
 
-        if (Input.GetKey(KeyCode.LeftArrow)) {
-            rotate(-TurnSpeed);
-        }
-        if (Input.GetKey(KeyCode.RightArrow)) {
-            rotate(TurnSpeed);
-        }
     }
 
     private void walking(float direction_forward, float direction_right) {
-        float x = (transform.right.x * MoveSpeed * Time.deltaTime * direction_right);
-        float y = 0f;
-        float z = (transform.forward.z * MoveSpeed * Time.deltaTime * direction_forward);
-        Vector3 direction = new Vector3(x, y, z);
-        rigidbody.MovePosition(rigidbody.position + direction);
+        
     }
 
-    private void rotate(float TurnSpeed) {
-        transform.Rotate(0, TurnSpeed * Time.deltaTime * 1f, 0);
 
-    }
-
-    /*    private void Jump() {
-            rigidbody.AddForce(0, JumpForce, 0);
-        }
-
-        private void OnCollisionStay(Collision collision) {
-            if (collision.gameObject.CompareTag(GameTags.Plaform) && Input.GetKeyDown(KeyCode.Space)) {
-                Jump();
-            }
-        }*/
-    }
+}
+    
