2b3d0f0fe20122649dcf2851e7711b7ab50742cc
brianmviana <brianmviana@gmail.com>
2021-05-02 19:49:30 -0300

    adding shoot effect and hit effect with particle system

--- Assets/Scripts/Player/PlayerShoot.cs
+++ Assets/Scripts/Player/PlayerShoot.cs
@@ -10,8 +10,7 @@ public class PlayerShoot: NetworkBehaviour {
     [SerializeField]
     private Camera cam;
 
-    private PlayerWeapon curretnWeapon;
-
+    private PlayerWeapon currentWeapon;
     private WeaponManager weaponManager;
 
     [SerializeField]
@@ -23,23 +22,21 @@ public class PlayerShoot: NetworkBehaviour {
             Debug.LogError("PlayerShoot: No camera referenced!");
             this.enabled = false;
         }
-
         weaponManager = GetComponent<WeaponManager>();
-
     }
 
     private void Update() {
-        curretnWeapon = weaponManager.GetCurretnWeapon();
 
+        currentWeapon = weaponManager.GetCurrentWeapon();
 
-        if (curretnWeapon.fireRate <= 0f) {
+        if (currentWeapon.fireRate <= 0f) {
             if (Input.GetButtonDown("Fire1")) {
                 Shoot();
             }
         }
         else {
             if (Input.GetButtonDown("Fire1")) {
-                InvokeRepeating("Shoot", 0f, 1f / curretnWeapon.fireRate);
+                InvokeRepeating("Shoot", 0f, 1f / currentWeapon.fireRate);
             }
             else if (Input.GetButtonUp("Fire1")) {
                 CancelInvoke("Shoot");
@@ -47,16 +44,53 @@ public class PlayerShoot: NetworkBehaviour {
         }
     }
 
+    // Is called on the server when a player shoots
+    [Command]
+    void CmdOnShoot() {
+        RpcDoShootEffect();
+    }
+
+    // Is called on all clients when we eed to a shoot effect
+    [ClientRpc]
+    void RpcDoShootEffect() {
+        weaponManager.GetCurrentGraphics().muzzleFlash.Play();
+    }
+
+    // Is called on the server when we hit something
+    // Takes in the hit point and the normal of the surface
+    [Command]
+    void CmdOnHit(Vector3 _pos, Vector3 _normal) {
+        RpcDoHitEffect(_pos, _normal);
+    }
+
+    // Is called on all clients
+    // Here we can spawn in cool effects
+    [ClientRpc]
+    void RpcDoHitEffect(Vector3 _pos, Vector3 _normal) {
+        GameObject _hitEffect = (GameObject)Instantiate(weaponManager.GetCurrentGraphics().hitEffectPrefab, _pos, Quaternion.LookRotation(_normal));
+        Destroy(_hitEffect, 2f);
+    }
+
+
     [Client]
     void Shoot() {
+
+        if (!isLocalPlayer) {
+            return;
+        }
+
+        // We are the shoot method on the server
+        CmdOnShoot();
+
         RaycastHit _hit;
 
-        Debug.Log("Shoot");
-
-        if (Physics.Raycast(cam.transform.position, cam.transform.forward, out _hit, curretnWeapon.range, mask)) {
+        if (Physics.Raycast(cam.transform.position, cam.transform.forward, out _hit, currentWeapon.range, mask)) {
             if (_hit.collider.tag == PLAYER_TAG) {
-                CmdPlayerShot(_hit.collider.name, curretnWeapon.damage);
+                CmdPlayerShot(_hit.collider.name, currentWeapon.damage);
             }
+
+            // We hit something, call the OnHit method on the server
+            CmdOnHit(_hit.point, _hit.normal);
         }
 
     }
